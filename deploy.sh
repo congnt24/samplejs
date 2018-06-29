68b92499f7f0de321f3bd0f7730692071bb8db2c
Cong Nguyen <congnt24@gmail.com>
2018-06-29 20:06:19 +0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,11 @@ REVISION=`aws ecs describe-task-definition --task-definition ${CI_PROJECT_NAME} 
 #Create or update service
 if [ "$SERVICES" == "" ]; then
   echo "entered existing service"
-  DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .services[].desiredCount`
+  DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} | jq .services[].desiredCount`
   if [ ${DESIRED_COUNT} = "0" ]; then
     DESIRED_COUNT="1"
   fi
-  aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
+  aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
 else
   echo "entered new service"
   aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER}
