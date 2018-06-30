ee284f75da392c9842e5bc0e2d362578b22dc295
Cong Nguyen <congnt24@gmail.com>
2018-06-30 08:53:02 +0700

    add logs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let routes = require('./routes');
 let app = express();
-app.use(routes);
+app.use('/test1', routes);
 
 app.listen(3000, () => {
     console.log('running')
