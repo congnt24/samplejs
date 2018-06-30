819bf1b7107a225cf05707bbf0ec0c47de0cab6b
Cong Nguyen <congnt24@gmail.com>
2018-06-30 08:21:48 +0700

    add logs

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 TASKDEF=task-definition.json
+SERVICEDEF=service-definition.json
 #FAMILY=`jq -r .family $TASKDEF`
 
 #Replace the build number and respository URI placeholders with the constants above
 sed -e "s;%REGION%;${AWS_DEFAULT_REGION};g" -e "s;%SERVICE_NAME%;${SERVICE_NAME};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI}/$REPO_NAME:latest;g" $TASKDEF > ${CI_PROJECT_NAME}.json
+sed -e "s;%SERVICE_NAME%;${SERVICE_NAME};g" $SERVICEDEF > ${CI_PROJECT_NAME}_SERVICE.json
 #Register the task definition in the repository
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${CI_PROJECT_NAME}.json
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} | jq '.services[] | select(.status == "ACTIVE")'`
@@ -21,5 +23,5 @@ if [ "$SERVICES" != "" ]; then
   aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
 else
   echo "entered new service"
-  aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER}
+  aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER} --cli-input-json file://${CI_PROJECT_NAME}_SERVICE.json
 fi
