bee291843b58a6aca5b59d0c293c3541256309be
Cong Nguyen <congnt24@gmail.com>
2018-06-30 17:22:34 +0700

    fix st

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,11 @@ let routes = express.Router();
 
 /* This route is used for Server monitor */
 routes.get('/', function (req, res) {
-    res.send('Hello world!! Home');
+    res.send('Hello world 1!! Home');
 });
 
 routes.get('/test', function (req, res) {
-    res.send('Hello world!!');
+    res.send('Hello world 1!!');
 });
 
 module.exports = routes;
