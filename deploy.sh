aba92472ac687db5228975d3a1c9030661dbeb64
Cong Nguyen <congnt24@gmail.com>
2018-06-30 22:15:06 +0700

    update ci

--- deploy.sh
+++ deploy.sh
@@ -19,10 +19,14 @@ if [ "$SERVICES" != "" ]; then
     DESIRED_COUNT="1"
   fi
   aws ecs update-service --cluster ${CLUSTER} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
+  echo "update service success"
 else
   echo "entered new service"
   TARGER_ARN=`aws elbv2 create-target-group --name ${SERVICE_NAME}-target --protocol HTTP --port 80 --vpc-id ${VPCID} | jq -r '.TargetGroups[].TargetGroupArn'`
+  echo "create target group success"
   aws elbv2 create-rule --listener-arn ${ALB_LISTENER} --priority $RANDOM --condition Field=path-pattern,Values='/${ROUTE}/*' --actions Type=forward,TargetGroupArn=${TARGER_ARN}
+  echo "create rule success"
   sed -e "s;%TARGETARN%;${TARGER_ARN};g" -e "s;%SERVICE_NAME%;${SERVICE_NAME};g" $SERVICEDEF > ${CI_PROJECT_NAME}_SERVICE.json
   aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER} --cli-input-json file://${CI_PROJECT_NAME}_SERVICE.json
+  echo "create service success"
 fi
