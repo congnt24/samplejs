8974e5f99ae7ea96642a54dd44548610fa6151e4
Cong Nguyen <congnt24@gmail.com>
2018-06-30 16:10:26 +0700

    fĩ st

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ let express = require('express');
 let routes = express.Router();
 
 /* This route is used for Server monitor */
+routes.get('/', function (req, res) {
+    res.send('Hello world!! Home');
+});
+
 routes.get('/test', function (req, res) {
     res.send('Hello world!!');
 });
