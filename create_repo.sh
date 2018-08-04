7415242ba6a4f09f7737cd7fcae195859ea9430b
Cong Nguyen <congnt24@gmail.com>
2018-08-04 11:47:20 +0700

    add dockerfile

--- create_repo.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-{
-    aws ecr describe-repositories --repository-name ${REPO_NAME}
-} || {
-    aws ecr create-repository --repository-name ${REPO_NAME} || echo "success"
-}
-#aws elbv2 create-target-group --name test1tg --protocol HTTP --port 80 --vpc-id vpc-b7e5bcdf
